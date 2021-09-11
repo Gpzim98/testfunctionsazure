e6731bf6e89e6045a21afbc13ca59fc5e4d92a84
Gregory <gpzim98@hotmail.com>
2021-09-11 12:26:27 +0100

    first commit - Create app function and a http trigger

---
+++ HttpTriggerMain.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Rhynow.Function
+{
+    public class Payload
+    {
+        public string test { get; set; }
+    }
+    public static class HttpTriggerMain
+    {
+        [Function("HttpTriggerMain")]
+        public static async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var logger = executionContext.GetLogger("HttpTriggerMain");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            Payload data = JsonConvert.DeserializeObject<Payload>(requestBody);
+            
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
